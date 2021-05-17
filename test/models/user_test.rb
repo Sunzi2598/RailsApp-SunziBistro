767eae290813a27ce964d41ae19846bcdc39a6b1
Sunzi2598 <sunzuianaalexandra@gmail.com>
2021-05-17 15:26:32 +0300

    Replaced the quotation marks

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,10 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(
-      name: "Example User", 
-      email: "user@example.com", 
-      password: "foobar", 
-      password_confirmation: "foobar"
+      name: 'Example User', 
+      email: 'user@example.com', 
+      password: 'foobar', 
+      password_confirmation: 'foobar'
     )
   end
 
@@ -15,22 +15,22 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'name should be present' do
-    @user.name = ""
+    @user.name = ''
     assert_not @user.valid?
   end
 
   test 'email should be present' do
-    @user.email = "     "
+    @user.email = '     '
     assert_not @user.valid?
   end 
 
   test 'name should not be too long' do
-    @user.name = "a" * 51
+    @user.name = 'a' * 51
     assert_not @user.valid?
   end
 
   test 'email should not be too long' do
-    @user.email = "a" * 244 + "@example.com"
+    @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
   end 
 
@@ -57,12 +57,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = " " * 6
+    @user.password = @user.password_confirmation = ' ' * 6
         assert_not @user.valid?
   end
 
   test 'password should have a minimum length' do 
-    @user.password = @user.password_confirmation = "a" * 5 
+    @user.password = @user.password_confirmation = 'a' * 5 
     assert_not @user.valid?
   end
 
