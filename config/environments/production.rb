a6c2faba6d9d5b00d044dfe72b3051c409aafae4
Sunzi2598 <sunzuianaalexandra@gmail.com>
2021-05-24 14:30:07 +0300

    Delete empty files, add empty lines, refactor routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,13 +52,13 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'sunzibistrorails.heroku.com',
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'sunzibistrorails.heroku.com',
+    authentication: :plain,
+    enable_starttls_auto: true
   }
   ActionMailer::Base.delivery_method = :smtp
 
